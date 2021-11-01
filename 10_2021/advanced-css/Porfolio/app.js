70de4776af934896a6f4e1e152c0fb879c5331a8
Rin9 <formygame38@gmail.com>
2021-11-01 09:48:28 -0400

    porfolio update

--- 10_2021/advanced-css/Porfolio/app.js
+++ 10_2021/advanced-css/Porfolio/app.js
@@ -1,7 +1,10 @@
 const hamburgerContainerEl = document.querySelector("#header__main-nav");
-const hamburgerEl = document.querySelector("#header__main-nav--hamburger");
-const linksEl = document.querySelector("#header__main-nav--links li");
+const hamburgerEl = document.querySelector(".header__main-nav--hamburger");
+const linksEl = document.querySelectorAll(".header__main-nav--links li");
 
 hamburgerEl.addEventListener("click", () => {
   hamburgerContainerEl.classList.toggle("clicked");
+  linksEl.forEach((link) => {
+    link.classList.toggle("fade");
+  });
 })
